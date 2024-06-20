964f9a30dd0dfca6a4c1942ea1fa2e08b8ebec5d
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-06-20 13:28:20 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("App is running")
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT || "8090";        //check if there is PORT envirenment variable. If not use use 8090 Port
+
+app.use(cors());
+
+app.use(express.json({ limit: "20mb" }))          //limits the size of json request body
+
+app.get("/", (req, res, next) => {                              //at the endpoint what want to see on the browser
+    res.send("<h2>Library Management System API<h2>");
+    next();
+})
+
+app.listen(PORT, () => {                                            //function for run the server
+    console.log(`Server is up and running on PORT ${PORT}`);
+});
