673cedaa857c68dde734cbb36dfefc411c90d23b
DasunFernando25 <146518662+DasunFernando25@users.noreply.github.com>
2024-07-01 20:33:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
+import 'dotenv/config';
 import express from "express";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
 import logger from "./utils/logger";
-import 'dotenv/config';
+import config from "./configs";
+import routesInit from './api/routes/routeIndex';
 import { connect } from "./utils/database.connection";
+import { googleAuth } from "./configs/google.config";
 
 const app = express();
 const PORT = process.env.PORT || "8090";        //check if there is PORT envirenment variable. If not use use 8090 Port
 
 app.use(cors());
 
-app.use(express.json({ limit: "20mb" }))          //limits the size of json request body
+app.use(express.json({ limit: "20mb" }));          //limits the size of json request body
+ 
+app.use(
+    session({
+        secret: config.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { 
+            secure: false,
+            expires: new Date(Date.now() + 10000),
+            maxAge:  10000
+        }
+    })
+)
+
+app.use(passport.initialize());
+
+app.use(passport.session());
 
 app.get("/", (req, res, next) => {                              //at the endpoint what want to see on the browser
-    res.send("<h2>Library Management System API<h2>");
+    // res.send("<h2>Library Management System API<h2>");
+    res.send("<a href = 'http://localhost:8090/auth/google'>Login with Google</a>");
     next();
 })
 
@@ -25,5 +48,9 @@ app.listen(PORT, () => {                                            //function f
     logger.info(`Server is up and running on PORT ${PORT}`);
 
     connect();
+
+    routesInit(app, passport);
+
+    googleAuth(passport);
 }); 
 
